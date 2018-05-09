cd23dfc54981dd622b39e690cd9b2b627436e183
jaymaxz <jagathmax2@gamil.com>
2018-05-09 09:57:04 +0530

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Main : MonoBehaviour {
+
+    SerialPort stream = new SerialPort("COM8", 9600);
+
+    // Use this for initialization
+    void Start()
+    {
+        stream.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = stream.ReadLine();
+        string[] vec3 = value.Split(',');
+        GetComponent<Renderer>().material.color = new Color(float.Parse(vec3[0])/255, float.Parse(vec3[1])/255, float.Parse(vec3[2])/255, float.Parse(vec3[3])/255);
+    }
+}
